ee9d99b6610205c0092ca2bb600ba6607e10a68a
dariocastillo11 <dario.4979102@gmail.com>
2025-09-09 14:24:41 -0300

    setear la cantidad de vulnerabilidades a pasar=2

--- pacman/vulnerable.c
+++ pacman/vulnerable.c
@@ -5,3 +5,19 @@ void vulnerable() {
     sprintf(command, "ls %s", getenv("USER"));  // uso inseguro de sprintf
     system(command);  // Horusec puede marcar esto como HIGH
 }
+
+
+
+void cmd_injection() {
+    char cmd[100];
+    char user[50] = "test";
+    sprintf(cmd, "ls %s", user);  // Inseguro
+    system(cmd);
+}
+
+// overflow de  buffer
+#include <string.h>
+void overflow() {
+    char buf[10];
+    strcpy(buf, "123456789012345");  // Demasiado largo para buf
+}
--- vulnerable.py
+++ vulnerable.py
@@ -2,3 +2,19 @@
 def test_vulnerability():
     password = "123456"  # Horusec debería detectarlo como vulnerabilidad
     print(f"Contraseña: {password}")
+
+
+API_KEY = "1234567890abcdef" 
+
+
+
+
+user_input = "2 + 2"
+result = eval(user_input)  # Riesgo de ejecución remota de código
+
+
+
+import pickle
+data = pickle.loads(b"some untrusted bytes")  # Puede ejecutar código malicioso
+
+
